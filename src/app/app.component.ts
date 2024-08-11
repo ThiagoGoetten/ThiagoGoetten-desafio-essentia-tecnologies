71adade792e70f49743e1ebe095f7ecb622ce741
Thiago <thiago.goetten@gmail.com>
2024-08-10 21:25:33 -0300

    refatoração de código

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { TodolistComponent } from "./todolist/todolist.component";
+
 
 @Component({
   selector: 'app-root',
   standalone: true, 
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [TodolistComponent]
+
 })
 export class AppComponent {
   title = 'to-do-list';
+
+  public tarefas="";
+  public items = ['item 1', 'item 2', 'item 3', 'item 4', 'item 5', 'item 6', 'item 7', 'item 8', 'item 9', 'item 10'];
+
+addTarefa() {
+
+  this.items.push(this.tarefas);
+
+}
+
 }
